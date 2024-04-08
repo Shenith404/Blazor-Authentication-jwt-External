2813fc3eeae6da3fdf324b9f1f5756edf84055c7
Shenith <shenithbandara@gmail.com>
2024-04-08 08:39:39 +0530

    ss

---
+++ BlazorAuthenticationJwt/BlazorAuthenticationJwt/Auth/Services/AuthService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+
+namespace BlazorAuthenticationJwt.Auth.Services
+{
+    public class AuthService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+     /*   public async Task<AuthResponse?> Login(AuthRequest request)
+        {
+            var result = await _httpClient.PostAsJsonAsync("auth/login", request);
+            if (result.IsSuccessStatusCode)
+            {
+                var content = await result.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<AuthResponse>(content);
+            }
+            else
+            {
+                return null;
+            }
+        }*/
+     
+    }
+}
